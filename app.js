0a076866c7cc152206fffe4a23891e3248c3fe65
Guido Pereira <stereopt@gmail.com>
2022-01-23 17:09:50 +0000

    Config File and Folder Creation

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const fs = require('fs/promises');
+const fs = require('fs');
 const path = require('path');
+const config = require('./config.json');
 
-const DOWNLOADS_PATH = path.parse('C:/Users/GuidoSP/Downloads');
+const DOWNLOADS_PATH = path.parse(config.downloads_folder_location);
+const SORTING_FOLDERS = config.sorting_folders;
 
 console.log("[Download Cleaner]");
 
-(async() => {
-  const download_files = await fs.readdir(path.join(DOWNLOADS_PATH.dir, DOWNLOADS_PATH.name));
-  console.log(download_files);
-})();
+// Setup Sorting Folders
+for(const folder of SORTING_FOLDERS) {
+  const sortFolder = path.join(DOWNLOADS_PATH.dir, DOWNLOADS_PATH.name, folder);
+  console.log(" -> " + sortFolder);
+  if(!fs.existsSync(sortFolder)) {
+    console.error("   -> Sorting Folder does not Exist!");
+    fs.mkdirSync(sortFolder);
+    console.log("   -> Created!");
+  }
+}
+
+// Sort Download Folder
+const download_files = fs.readdirSync(path.join(DOWNLOADS_PATH.dir, DOWNLOADS_PATH.name));
+console.log(download_files);
